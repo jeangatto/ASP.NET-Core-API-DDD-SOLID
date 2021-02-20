4f23ff96cecfb22e1fb751c157fa2855b49fb057
Jean Francisco Flores Gatto <jffgatto@paschoalotto.com.br>
2021-02-20 11:17:30 -0300

    chore: configuração inicial da solução

---
+++ src/SGP.Shared/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SGP.Shared.Entities
+{
+    /// <summary>
+    /// Entidade base com o tipo da chave em guid.
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
---
+++ src/SGP.Shared/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,9 @@
+﻿namespace SGP.Shared.Interfaces
+{
+    /// <summary>
+    /// [Interface de Marcador] Indica que a classe de entidade é uma raiz de agregação.
+    /// </summary>
+    public interface IAggregateRoot
+    {
+    }
+}
