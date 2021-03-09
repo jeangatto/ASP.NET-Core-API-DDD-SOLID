967ff0f4a7c3e70f598bf8a0d10246a148b86b6c
Jean Francisco Flores Gatto <jffgatto@paschoalotto.com.br>
2021-03-09 20:55:23 -0300

    docs: sumário adicionado na interface IResult

--- src/SGP.Shared/Results/IResult.cs
+++ src/SGP.Shared/Results/IResult.cs
@@ -2,12 +2,26 @@
 {
     public interface IResult
     {
+        /// <summary>
+        /// Mensagem do resultado (opcional).
+        /// </summary>
         string Message { get; }
+
+        /// <summary>
+        /// Indica se o resultado foi bem-sucedido.
+        /// </summary>
         bool Succeeded { get; }
     }
 
+    /// <summary>
+    /// Resultado com dados.
+    /// </summary>
+    /// <typeparam name="T">O tipo do objeto.</typeparam>
     public interface IResult<out T> : IResult
     {
+        /// <summary>
+        /// Dados do resultado.
+        /// </summary>
         T Data { get; }
     }
 }
