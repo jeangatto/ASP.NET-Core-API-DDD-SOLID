1d11a904e57c47ea06807425074562c45db4e3c9
Jean Gatto <jean_gatto@hotmail.com>
2022-04-30 16:43:23 -0300

    feat: atualizado a biblioteca FluentValidation

--- src/SGP.Shared/Extensions/ValidatorConfigurationExtensions.cs
+++ src/SGP.Shared/Extensions/ValidatorConfigurationExtensions.cs
@@ -5,8 +5,5 @@ namespace SGP.Shared.Extensions;
 public static class ValidatorConfigurationExtensions
 {
     public static void Configure(this ValidatorConfiguration configuration)
-    {
-        configuration.CascadeMode = CascadeMode.Continue;
-        configuration.DisplayNameResolver = (_, member, _) => member?.Name;
-    }
+        => configuration.DisplayNameResolver = (_, member, _) => member?.Name;
 }
