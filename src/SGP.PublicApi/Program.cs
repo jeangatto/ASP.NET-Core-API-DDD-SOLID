738a185a1bda5c13ffd9c4395fdfcf0d65b09e8f
Jean Gatto <jean_gatto@hotmail.com>
2022-06-02 20:53:35 -0300

    feat: removido usings não utilizados

--- src/SGP.PublicApi/Program.cs
+++ src/SGP.PublicApi/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SGP.Infrastructure.Context;
-using SGP.PublicApi.Extensions;
 
 namespace SGP.PublicApi;
 
--- src/SGP.PublicApi/Startup.cs
+++ src/SGP.PublicApi/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using SGP.Application;
 using SGP.Infrastructure;
 using SGP.PublicApi.Extensions;
