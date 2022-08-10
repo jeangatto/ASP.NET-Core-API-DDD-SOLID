0994202253db10318b9e68418d02fd15489405a9
Jean Gatto <jean_gatto@hotmail.com>
2022-08-10 14:38:03 -0300

    fix: fixed SonarCube rule => csharpsquid:S3376

--- src/SGP.Shared/Validation/RequiredGreaterThanZero.cs
+++ src/SGP.Shared/Validation/RequiredGreaterThanZero.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace SGP.Shared.Validation;
 
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
-public sealed class RequiredGreaterThanZero : ValidationAttribute
+public sealed class RequiredGreaterThanZeroAttribute : ValidationAttribute
 {
     public override bool IsValid(object value)
         => value != null && int.TryParse(value.ToString(), out var result) && result > 0;
