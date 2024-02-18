0b21de7685d36e23d4be3c3187afa522b648c6ea
Jean Gatto <jean_gatto@hotmail.com>
2024-02-18 08:34:39 -0300

    chore: update nuget packages

--- src/SGP.PublicApi/Models/ApiResponse.cs
+++ src/SGP.PublicApi/Models/ApiResponse.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Http;
 
 namespace SGP.PublicApi.Models;
@@ -24,7 +23,7 @@ public class ApiResponse
     /// <summary>
     /// Lista com os erros da requisição se não for bem-sucedida.
     /// </summary>
-    public IEnumerable<ApiError> Errors { get; private init; } = Enumerable.Empty<ApiError>();
+    public IEnumerable<ApiError> Errors { get; private init; } = [];
 
     #endregion
 
